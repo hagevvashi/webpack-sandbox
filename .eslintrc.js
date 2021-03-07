53c4b0c038643567795f93ade3d959a402b1b7b1
hagevvashi <hagevvashi@gmail.com>
2021-03-07 16:27:37 +0900

    chore: refactor eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,9 @@
-const { resolve } = require('path');
-
 module.exports = {
-  extends: ['airbnb-base', 'prettier'],
-  plugins: ['prettier', '@typescript-eslint'],
+  base: true,
+  extends: ["prettier"],
+  plugins: ["@typescript-eslint"],
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    sourceType: 'module',
-    project: resolve(__dirname, 'tsconfig.json')
-  },
-  rules: {
-    'prettier/prettier': ['error'],
-    'no-unused-vars': 'off',
-    camelcase: 'off',
-    '@typescript-eslint/no-unused-vars': 'error',
-    '@typescript-eslint/camelcase': ['error', { properties: 'never' }]
-  },
-  env: {
-    node: true,
-    es6: true,
-    commonjs: true,
-    jest: true
+    project: "./tsconfig.json",
   }
 };
